7f4b45db1dc884a8b04a1ccc8acc5a0e9ed79d7f
ZerheriFatimaZahrae <fatimazahraezerheripc@email.com>
2024-05-06 16:19:16 +0100

    second  commit

---
+++ frontend-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {MatToolbar} from "@angular/material/toolbar";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminTemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
